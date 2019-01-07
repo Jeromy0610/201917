08cf7f687477b9b313ccf537e70fd4d862de9212
Jeromy0610 <b1065114@mail.nuk.edu.tw>
2019-01-07 15:22:10 +0800

    change4

--- main.c
+++ main.c
@@ -4,11 +4,13 @@
 int main()
 {
     int a,b,c;
+    printf("test !!\n");
     printf("Hello world!\n");
     for(a=0; a<6; a++){
         b=a+1;
         c=c+b;
     }
     printf("c:%d",c);
+
     return 0;
 }
